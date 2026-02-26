737577993a7958c8932e80b66fd2cda7525ad2fc
shimoyuya <3074609852@qq.com>
2026-02-26 16:59:00 +0800

    引入 EQS 系统，使用 EQS 让 AI 在玩家周围寻找“最佳攻击点”或“最佳掩体”。例如：近战敌人把玩家围成一圈，而不是全部重叠在一起（群殴逻辑）。

---
+++ Source/PanLing/EnvQueryContext_Target.cpp
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnvQueryContext_Target.h"
+#include "EnvironmentQuery/Items/EnvQueryItemType_Actor.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Blueprint/AIBlueprintHelperLibrary.h"
+#include "AIController.h"
+#include "EnvironmentQuery/EnvQueryTypes.h"
+
+void UEnvQueryContext_Target::ProvideContext(FEnvQueryInstance& QueryInstance, FEnvQueryContextData& ContextData) const
+{
+	// 1. 获取是谁在发起这个 EQS 查询 (通常是 AI 控制器或者 AI Pawn)
+	AActor* QueryOwner = Cast<AActor>(QueryInstance.Owner.Get());
+	if (!QueryOwner) return;
+
+	// 2. 尝试获取 AI 控制器
+	AAIController* AICon = nullptr;
+	if (APawn* OwnerPawn = Cast<APawn>(QueryOwner))
+	{
+		// 如果 Owner 是 Pawn，获取它身上的 Controller
+		AICon = Cast<AAIController>(OwnerPawn->GetController());
+	}
+	else
+	{
+		// 否则 Owner 本身可能就是 Controller
+		AICon = Cast<AAIController>(QueryOwner);
+	}
+
+	// 3. 如果成功获取到了 AIController 以及它的黑板组件
+	if (AICon && AICon->GetBlackboardComponent())
+	{
+		// 读取我们之前在感知系统中存入的 "TargetActor"
+		UObject* TargetObject = AICon->GetBlackboardComponent()->GetValueAsObject(FName("TargetActor"));
+		AActor* TargetActor = Cast<AActor>(TargetObject);
+
+		if (TargetActor)
+		{
+			// 4. 将获取到的玩家 Actor 包装进 EQS 的 ContextData 中！
+			// 这行代码是 EQS 引擎底层的固定写法，告诉 EQS "目标就是这个 Actor"
+			UEnvQueryItemType_Actor::SetContextHelper(ContextData, TargetActor);
+		}
+	}
+}
---
+++ Source/PanLing/EnvQueryContext_Target.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnvironmentQuery/EnvQueryContext.h"
+#include "EnvQueryContext_Target.generated.h"
+
+/**
+ * 自定义 EQS 上下文：从 AI 的黑板中获取名为 "TargetActor" 的对象
+ * 这样 EQS 生成器就可以围绕这个目标生成点。
+ */
+UCLASS()
+class PANLING_API UEnvQueryContext_Target : public UEnvQueryContext
+{
+	GENERATED_BODY()
+	
+public:
+	// 虚幻引擎 EQS 系统的核心接口，用于返回我们需要的上下文数据
+	virtual void ProvideContext(FEnvQueryInstance& QueryInstance, FEnvQueryContextData& ContextData) const override;
+
+};
