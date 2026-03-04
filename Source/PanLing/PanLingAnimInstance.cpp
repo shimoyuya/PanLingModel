23e38b63055cf06bd99aceaba035966198de36b4
shimoyuya <3074609852@qq.com>
2026-03-04 14:06:10 +0800

    使用传统移动状态机来制作角色和敌人的动画,创建 C++ 动画实例基类 (UPanLingAnimInstance)：它继承自 UAnimInstance，负责在每一帧（NativeUpdateAnimation）获取拥有者的速度（Speed）、掉落状态（bIsFalling）和移动意图（bShouldMove）

---
+++ Source/PanLing/PanLingAnimInstance.cpp
@@ -0,0 +1,42 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PanLingAnimInstance.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void UPanLingAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	// 尝试获取拥有这个动画蓝图的 Pawn，并转换为 ACharacter
+	OwnerCharacter = Cast<ACharacter>(TryGetPawnOwner());
+
+	if (OwnerCharacter)
+	{
+		// 缓存移动组件
+		CharacterMovement = OwnerCharacter->GetCharacterMovement();
+	}
+}
+
+void UPanLingAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	// 如果拥有者为空，说明还没初始化好，直接返回
+	if (OwnerCharacter == nullptr || CharacterMovement == nullptr)
+	{
+		return;
+	}
+
+	// 1. 计算水平速度 (忽略 Z 轴上下掉落的速度)
+	FVector Velocity = OwnerCharacter->GetVelocity();
+	Velocity.Z = 0.f;
+	Speed = Velocity.Size();
+
+	// 2. 检查是否在空中
+	bIsFalling = CharacterMovement->IsFalling();
+
+	// 3. 检查是否有移动意图 (速度大于一定值，且有加速度输入)
+	bShouldMove = (Speed > 3.f) && (CharacterMovement->GetCurrentAcceleration().Size() > 0.f);
+}
---
+++ Source/PanLing/PanLingAnimInstance.h
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "PanLingAnimInstance.generated.h"
+
+/**
+ * 通用动画实例基类，适用于玩家和敌人
+ */
+UCLASS()
+class PANLING_API UPanLingAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+public:
+	// 动画初始化时调用（类似 BeginPlay）
+	virtual void NativeInitializeAnimation() override;
+
+	// 每帧更新动画时调用（类似 Tick）
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+protected:
+	// 缓存的角色指针，避免每帧去获取
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	class ACharacter* OwnerCharacter;
+
+	// 缓存的移动组件指针
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	class UCharacterMovementComponent* CharacterMovement;
+
+	// 当前水平移动速度
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	float Speed;
+
+	// 是否处于空中（跳跃或掉落）
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	bool bIsFalling;
+
+	// 是否有移动输入（用于区分是被推开还是主动移动）
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	bool bShouldMove;
+};
